2a10afa5b42160ff57c21d9cf0e05f28f78e8a44
Mouyna Leo <lmouyna@tc405-112-10.insa-lyon.fr>
2017-06-06 09:19:02 +0200

    Add Agents Protocol and Applications

--- TP3_NetworkSimulationNs/simple.tcl
+++ TP3_NetworkSimulationNs/simple.tcl
@@ -12,3 +12,21 @@ for {set i 0} {$i < $nbNodes} {incr i} {
 
 #Create a symetric bidirectional link
 $ns duplex-link $n0 $n1 2Mb 10ms DropTail;
+
+#Create a new agent protocol
+set udp [new Agent/UDP];
+set sink [new Agent/Null];
+
+#Attach a protocol to a node
+$ns attach-agent $n0 $udp;
+$ns attach-agent $n1 $sink;
+
+#connect agents
+$ns connect $udp $sink;
+
+#Add an application
+set cbr [new Application/Traffic/CBR];
+$cbr attach-agent $udp;
+#Démarage décalé de 10s
+$ns at 10.0 "$cbr start";
+
